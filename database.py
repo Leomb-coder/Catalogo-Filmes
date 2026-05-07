f1e40be61049ca60b7d08a4e0f9effd88097408a
Leomb-Coder <leombernardoni@gmail.com>
2026-05-07 15:48:19 -0300

    Ajustei database para Render e Neon

--- database.py
+++ database.py
@@ -10,14 +10,10 @@ def get_connection():
     host = os.environ.get("DB_HOST")
     print("host------------------: ", host)
     conn = psycopg2.connect(
-        # host= os.environ.get("DB_HOST"),
-        # database= os.environ.get("DB_NAME"),
-        # user= os.environ.get("DB_USER"),
-        # password= os.environ.get("DB_PASSWORD"),
-        host='127.0.0.1',
-        database='catalogo_filmes',
-        user='postgres',
-        password='1234',
+        host= os.environ.get("DB_HOST"),
+        database= os.environ.get("DB_NAME"),
+        user= os.environ.get("DB_USER"),
+        password= os.environ.get("DB_PASSWORD"),
     )
     return conn
 
